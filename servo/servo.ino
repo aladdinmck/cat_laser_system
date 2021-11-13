574fd102cd2da51cb0fa30707573c444a637de8f
aladdin-mck <aladdin320@yahoo.com>
2021-11-12 21:48:10 -0500

    fixed servo range

--- servo/servo.ino
+++ servo/servo.ino
@@ -9,20 +9,22 @@ int pos2 = 0;
 void setup() {
   servo_1.attach(9);
   servo_2.attach(10);
+    Serial.begin(9600);
+
 }
 
 
 void loop () {
-  for (pos1 = 0, pos2 = 0; pos1 <= 180 && pos2 <= 180; pos1++, pos2++) {
+  for (pos1 = 95, pos2 = 95; pos1 <= 150 && pos2 <= 150; pos1++, pos2++) {
     servo_1.write(pos1);
     servo_2.write(pos2);
-    delay(15);  
+    delay(1000);  
   }    
 
-  for (pos1 = 180, pos2 = 180; pos1 > 0 && pos2 > 0; pos1--, pos2--) {
+  for (pos1 = 150, pos2 = 150; pos1 > 95 && pos2 > 95; pos1--, pos2--) {
      servo_1.write(pos1);
      servo_2.write(pos2);
-     delay(15);
+     delay(1000);
   }
  
 }
